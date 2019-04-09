0043c0fcc5c6abe42d052750cdaa2c42b6e58380
Stelios Magalios <dai18034@uom.edu.gr>
2019-04-09 04:58:24 +0300

    initial commit
    Signed-off-by: steliosmagalios <stelios.magalios1@gmail.com>

---
+++ src/main/java/gr/uom/softeng/team2/argoapp/Main.java
@@ -0,0 +1,16 @@
+package gr.uom.softeng.team2.argoapp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
