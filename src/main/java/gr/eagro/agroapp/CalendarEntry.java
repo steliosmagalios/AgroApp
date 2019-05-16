eafd5fe0151203d534e035bd49c1c6b4fdfe2bb8
gkouvas3 <gkouvas3@hotmail.gr>
2019-05-16 11:38:31 +0300

    Merge branch 'master' of C:\Users\gkouv\IdeaProjects\AgroApp1 with conflicts.

---
+++ src/main/java/gr/eagro/agroapp/CalendarEntry.java
@@ -0,0 +1,55 @@
+package gr.eagro.agroapp;
+
+public class CalendarEntry {
+	private int week;
+	private int month;
+	private int year;
+	private String displayText;
+	
+	public CalendarEntry(int week, int month, int year, String displayText) {
+		this.week = week;
+		this.month = month;
+		this.year = year;
+		this.displayText = displayText;
+	}
+	
+
+	public int getWeek() {
+		return week;
+	}
+
+	public void setWeek(int week) {
+		this.week = week;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getDisplayText() {
+		return displayText;
+	}
+
+	public void setDisplayText(String displayText) {
+		this.displayText = displayText;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return this.displayText;
+	}
+}
