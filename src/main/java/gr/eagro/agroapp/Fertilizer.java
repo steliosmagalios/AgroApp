92a8a70e6fa194a5d324eb08fd8eef55fcb69c7b
gkouvas3 <gkouvas3@hotmail.gr>
2019-05-18 18:40:44 +0300

    changed the way method "getInfo" works, changed variables names at method "calculateCost" and added the structure of the method "calculateQuantity"

---
+++ src/main/java/gr/eagro/agroapp/Fertilizer.java
@@ -0,0 +1,51 @@
+package gr.eagro.agroapp;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Fertilizer {
+    private String id;
+    private double quantity;
+    private double cost;
+
+    public Fertilizer(String id, double quantity, double cost) {
+        this.id = id;
+        this.quantity = quantity;
+        this.cost = cost;
+
+    }
+
+    public double calculateCost(double fertilizerQuantity, double cost) {
+        double totalcost;
+        totalcost = fertilizerQuantity * cost;
+        return totalcost;
+    }
+
+    //public double calculateQuantity(double plantQuiantity) {
+        /* somehow i have to return the quantinty of the fertilizer required, based on the quantity of the plants*/
+
+    //}
+
+    public File getInfo(){
+
+        File f = new File("./info/fertilizer/" + this.id + ".txt" );
+        try {
+            Scanner scan = new Scanner(f);
+
+            String line = scan.nextLine();
+            if(line.equals(null)){
+                return null;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return f;
+    }
+
+
+
+
+    }
+
